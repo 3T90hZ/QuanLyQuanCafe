989a493078420185a7501a3d30b2f3c940b3bc28
3T90hZ <165739979+3T90hZ@users.noreply.github.com>
2025-03-29 14:55:49 +0700

    Add files via upload

---
+++ Database/Proc.sql
@@ -0,0 +1,362 @@
+﻿use QL_QuanCafe;
+------------------------------------------------------------------------PROCEDURE-----------------------------------------------------------------
+create PROCEDURE USP_Login
+    @username VARCHAR(100),
+    @password VARCHAR(100)
+AS
+BEGIN
+    SELECT TOP 1 *
+    FROM Account ac
+    WHERE ac.username = @username AND ac.password = @password;
+END;
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+create procedure USP_GetAccountByUserName
+@username nvarchar(100)
+as
+begin
+	select * from dbo.Account where UserName = @username
+end;
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+DROP PROCEDURE IF EXISTS USP_LoadTableList
+CREATE PROCEDURE USP_LoadTableList
+As
+Begin
+	SELECT * FROM TableFood;
+End;
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+create procedure USP_InsertBill
+@idTable int
+as
+begin
+insert dbo.Bill(TimeCheckIn, TimeCheckOut,discount, idTable, status)
+values (CONVERT(VARCHAR(30), GETDATE(), 120), null, 0, @idTable, 0)
+end;
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+create proc USP_InsertBillInfo
+@idBill int, @idFood int, @count int
+as
+begin
+	declare @isExistBillInfo int, @foodCount int = 0
+	
+	select @isExistBillInfo = id, @foodCount = bi.count 
+	from dbo.BillInfo as bi 
+	where idBill = @idBill and idFood = @idFood
+	
+	if(@isExistBillInfo > 0)
+	Begin
+		declare @newCount int = @foodCount + @count
+		if(@newCount > 0)
+			update dbo.BillInfo set count = @foodCount + @count where idFood = @idFood
+		else
+			delete dbo.BillInfo where idBill = @idBill and idFood = @idFood
+			
+	end;
+	else
+	begin
+		if(@count > 0)
+			insert dbo.BillInfo(idBill, idFood, count)
+			values(@idBill, @idFood, @count)
+	end
+end;
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+create procedure USP_SwitchTable
+@idTable1 int, @idTable2 int
+as
+begin
+	declare @idFirstBill int, @idSecondBill int
+	declare @isFirstTableEmpty int = 1, @isSecondTableEmpty int = 1
+
+
+	select @idSecondBill = id from dbo.Bill where idTable = @idTable2 and status = 0 
+	select @idFirstBill = id from dbo.Bill where idTable = @idTable1 and status = 0 
+
+	if(@idFirstBill is null)
+	begin
+		insert dbo.Bill(TimeCheckIn, TimeCheckOut, idTable, status)
+		values (CONVERT(VARCHAR(30), GETDATE(), 120), null, @idTable1, 0)
+		select @idFirstBill = max(id) from dbo.Bill where idTable = @idTable1 and status = 0
+	end
+	select @isFirstTableEmpty = count(*) from dbo.BillInfo where idBill = @idFirstBill
+	if(@idSecondBill is null)
+	begin
+		insert dbo.Bill(TimeCheckIn, TimeCheckOut, idTable, status)
+		values (CONVERT(VARCHAR(30), GETDATE(), 120), null, @idTable2, 0)
+		select @idSecondBill = max(id) from dbo.Bill where idTable = @idTable2 and status = 0
+	end
+	select @isSecondTableEmpty = count(*) from dbo.BillInfo where idBill = @idSecondBill
+
+	select id into IDBillInfoTable from dbo.BillInfo where idBill = @idSecondBill
+
+	update dbo.BillInfo set idBill = @idSecondBill where idBill = @idFirstBill
+	update dbo.BillInfo set idBill = @idFirstBill where id in (select * from IDBillInfoTable)
+
+	drop table IDBillInfoTable
+	if(@isFirstTableEmpty = 0)
+		update dbo.TableFood set status = N'Trống' where id = @idTable2
+	if(@isSecondTableEmpty = 0)
+		update dbo.TableFood set status = N'Trống' where id = @idTable1
+end;
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+create procedure USP_GetListBillByDateAndPage
+@checkIn datetime, @checkOut datetime, @page int
+as
+begin
+	declare @pageRows int = 10
+	declare @selectedRows int = @pageRows * @page
+	declare @exceptRows int = (@page - 1) * @pageRows
+		
+		select t.name as [Tên bàn], b.TimeCheckIn as [Vào], b.TimeCheckOut as [Ra], b.discount as [Giảm giá(%)], b.totalPrice as[Tổng tiền(vnd)], b.username as [user] into SortBillByTimeCheckOut
+		from dbo.Bill as b, dbo.TableFood as t
+		where TimeCheckIn >= @checkIn and TimeCheckOut <= @checkOut and b.status = 1 and t.id = b.idTable
+		order by TimeCheckOut DESC
+
+		select top (@selectedRows) * from SortBillByTimeCheckOut
+		except
+		select top (@exceptRows) * from SortBillByTimeCheckOut
+
+		drop table SortBillByTimeCheckOut
+end;
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+create procedure USP_GetNumBillByDate
+@checkIn datetime, @checkOut datetime
+as
+begin
+	select Count(*)
+	from dbo.Bill as b, dbo.TableFood as t
+	where TimeCheckIn >= @checkIn and TimeCheckOut <= @checkOut and b.status = 1 and t.id = b.idTable
+end;
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+  
+create procedure USP_UpdateAccount
+@userName nvarchar(100), @displayName nvarchar (100), @password nvarchar(100), @newPassword nvarchar(100)
+as
+begin
+	declare @isRightPass int
+
+	select @isRightPass = count(*) from dbo.Account where UserName = @userName and Password = @password
+	if(@isRightPass = 1)
+	begin
+		if(@newPassword is null or @newPassword = '')
+		begin
+			update dbo.Account set DisplayName = @displayName where UserName = @userName	
+		end
+		else
+			update dbo.Account set DisplayName = @displayName, Password = @newPassword where UserName = @userName
+	end
+end;
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+create procedure USP_SearchFoodByName
+@foodName NVARCHAR(100)
+as
+begin
+declare @fn nvarchar(100)
+SELECT @fn =  dbo.non_unicode_convert(@foodName)
+SELECT * FROM dbo.Food WHERE dbo.non_unicode_convert(name) LIKE  '%' + @fn + '%'
+end
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+create procedure USP_GetRevenueByDate
+@checkIn datetime, @checkOut datetime
+as
+begin	
+		select sum(b.totalPrice)
+		from dbo.Bill as b
+		where TimeCheckIn >= @checkIn and TimeCheckOut <= @checkOut and b.status = 1 
+end;
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+create procedure USP_GetBestSelling
+@checkIn datetime, @checkOut datetime
+as
+begin
+select top 1 idFood as id into Summ 
+from dbo.BillInfo as bi, dbo.Bill as b
+where bi.idBill = b.id and b.TimeCheckIn >= @checkIn and b.TimeCheckOut <= @checkOut and b.status = 1
+group by idFood
+order by SUM(count) desc
+
+select name from Food, Summ where Food.id = Summ.id
+	drop table Summ
+end;
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+create procedure USP_GetNumBestSelling
+@checkIn datetime, @checkOut datetime
+as
+begin
+select top 1 SUM(count) 
+from dbo.BillInfo as bi, dbo.Bill as b
+where bi.idBill = b.id and b.TimeCheckIn >= @checkIn and b.TimeCheckOut <= @checkOut and b.status = 1
+group by idFood
+order by SUM(count) desc
+end;
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+create procedure USP_GetWorstSelling
+@checkIn datetime, @checkOut datetime
+as
+begin
+select idFood as id, sum(bi.count) as sum into Summ 
+from dbo.BillInfo as bi, dbo.Bill as b
+where bi.idBill = b.id and b.TimeCheckIn >= @checkIn and b.TimeCheckOut <= @checkOut and b.status = 1
+group by idFood 
+
+select top 1 name from Food left outer join Summ on Food.id = Summ.id
+order by sum asc
+	drop table Summ
+end;
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+create procedure USP_GetNumWorstSelling
+@checkIn datetime, @checkOut datetime
+as
+begin
+select idFood as id, sum(bi.count) as sum into Summ 
+from dbo.BillInfo as bi, dbo.Bill as b
+where bi.idBill = b.id and b.TimeCheckIn >= @checkIn and b.TimeCheckOut <= @checkOut and b.status = 1
+group by idFood 
+
+
+select top 1 COALESCE(sum, 0) from Food left outer join Summ on Food.id = Summ.id
+order by sum asc
+	drop table Summ
+end;
+---------------------------------------------------------------------------------TRIGGER-------------------------------------------------------------------
+
+alter trigger UTG_UpdateBillInfo
+on dbo.BillInfo after insert, update
+as
+begin
+	declare @idBill int
+
+	select @idBill = idBill From inserted
+
+	declare @idTable int
+
+	select @idTable = idTable from dbo.Bill where id = @idBill and status = 0
+
+	declare @count int
+	select @count = count(*) from dbo.BillInfo where idBill = @idBill
+
+	if(@count > 0)
+	begin
+	update dbo.TableFood set status = N'Có người' where id = @idTable
+	end
+	else
+	begin
+		update dbo.TableFood set status = N'Trống' where id = @idTable
+	end
+end;	
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+create TRIGGER UTG_DeleteCategory ON FoodCategory
+instead of DELETE
+AS
+BEGIN
+	DECLARE @id int
+	
+	SELECT @id = id FROM deleted
+	delete dbo.BillInfo where idFood in (select id from dbo.Food where idCategory = @id)
+	delete dbo.Food where idCategory = @id
+	delete dbo.FoodCategory where id = @id
+END;
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+create trigger UTG_UpdateBill
+on dbo.Bill for update
+as
+begin
+	declare @idBill int
+
+	select @idBill = id from inserted
+
+	declare @idTable int
+
+	select @idTable = idTable from dbo.Bill where id = @idBill
+	declare @count int = 0	
+	select @count = count(*) from dbo.Bill where idTable = @idTable and status = 0
+	if(@count = 0)
+		update dbo.TableFood set status = N'Trống' where id = @idTable
+end;
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+create trigger UTG_DeleteBillInfo
+on BillInfo after delete
+as
+begin
+	declare @idBillInfo int 
+	declare @idBill int
+	select @idBillInfo = id, @idBill = deleted.idBill from Deleted
+ 
+	declare @idTable int 
+	select @idTable = idTable from dbo.Bill where id = @idBill
+
+	declare @count int = 0 
+	select @count = count(*) from dbo.BillInfo, dbo.Bill where Bill.id = @idBill and Bill.id = BillInfo.idBill and Bill.status = 0
+	if(@count = 0)
+		delete dbo.Bill where id = @idBill
+		update dbo.TableFood set status = N'Trống' where id =  @idTable
+end;
+
+---------------------------------------------------------------------------FuncTion---------------------------------------------------------------------
+CREATE FUNCTION [dbo].[non_unicode_convert](@inputVar NVARCHAR(MAX) )
+RETURNS NVARCHAR(MAX)
+AS
+BEGIN    
+    IF (@inputVar IS NULL OR @inputVar = '')  RETURN ''
+   
+    DECLARE @RT NVARCHAR(MAX)
+    DECLARE @SIGN_CHARS NCHAR(256)
+    DECLARE @UNSIGN_CHARS NCHAR (256)
+ 
+    SET @SIGN_CHARS = N'ăâđêôơưàảãạáằẳẵặắầẩẫậấèẻẽẹéềểễệếìỉĩịíòỏõọóồổỗộốờởỡợớùủũụúừửữựứỳỷỹỵýĂÂĐÊÔƠƯÀẢÃẠÁẰẲẴẶẮẦẨẪẬẤÈẺẼẸÉỀỂỄỆẾÌỈĨỊÍÒỎÕỌÓỒỔỖỘỐỜỞỠỢỚÙỦŨỤÚỪỬỮỰỨỲỶỸỴÝ' + NCHAR(272) + NCHAR(208)
+    SET @UNSIGN_CHARS = N'aadeoouaaaaaaaaaaaaaaaeeeeeeeeeeiiiiiooooooooooooooouuuuuuuuuuyyyyyAADEOOUAAAAAAAAAAAAAAAEEEEEEEEEEIIIIIOOOOOOOOOOOOOOOUUUUUUUUUUYYYYYDD'
+ 
+    DECLARE @COUNTER int
+    DECLARE @COUNTER1 int
+   
+    SET @COUNTER = 1
+    WHILE (@COUNTER <= LEN(@inputVar))
+    BEGIN  
+        SET @COUNTER1 = 1
+        WHILE (@COUNTER1 <= LEN(@SIGN_CHARS) + 1)
+        BEGIN
+            IF UNICODE(SUBSTRING(@SIGN_CHARS, @COUNTER1,1)) = UNICODE(SUBSTRING(@inputVar,@COUNTER ,1))
+            BEGIN          
+                IF @COUNTER = 1
+                    SET @inputVar = SUBSTRING(@UNSIGN_CHARS, @COUNTER1,1) + SUBSTRING(@inputVar, @COUNTER+1,LEN(@inputVar)-1)      
+                ELSE
+                    SET @inputVar = SUBSTRING(@inputVar, 1, @COUNTER-1) +SUBSTRING(@UNSIGN_CHARS, @COUNTER1,1) + SUBSTRING(@inputVar, @COUNTER+1,LEN(@inputVar)- @COUNTER)
+                BREAK
+            END
+            SET @COUNTER1 = @COUNTER1 +1
+        END
+        SET @COUNTER = @COUNTER +1
+    END
+    -- SET @inputVar = replace(@inputVar,' ','-')
+    RETURN @inputVar
+END;
---
+++ Database/QLCF.sql
@@ -0,0 +1,158 @@
+﻿use QL_QuanCafe
+
+drop table TableFood
+create table TableFood
+(
+id int identity primary key,
+name nvarchar(100) not null default N' bàn chưa có tên',
+status nvarchar(100) not null default N'Trống'
+);
+
+
+create table AccountType
+(
+id int primary key,
+name nvarchar(100)
+);
+
+
+create table Account
+(
+UserName nvarchar(100) primary key,
+DisplayName nvarchar(100) not null default N'User',
+Password nvarchar(100) not null default 1,
+Type int not null default 0 
+foreign key (Type) references dbo.AccountType(id)
+);
+
+drop table FoodCategory
+create table FoodCategory
+(
+id int identity primary key,
+name nvarchar(100) not null default N'Chưa đặt tên'
+);
+
+drop table Food
+create table Food
+(
+id int identity primary key,
+name nvarchar(100) not null default N'Chưa đặt tên',
+idCategory int not null,
+price float not null default 0,
+
+foreign key (idCategory) references dbo.FoodCategory(id)
+);
+
+
+drop table Bill
+create table Bill
+(
+id int identity primary key,
+TimeCheckIn datetime not null Default CONVERT(VARCHAR(30), GETDATE(), 120),
+TimeCheckOut datetime,
+discount int default 0,
+idTable int not null,
+status int not null default 0,
+totalPrice float,
+username nvarchar(100) null, 
+foreign key (username) references dbo.Account(username),
+foreign key (idTable) References dbo.TableFood(id)
+);
+
+
+drop table BillInfo
+create table BillInfo
+(
+id int identity primary key,
+idBill int not null,
+idFood int not null,
+count int not null default 0
+
+foreign key (idBill) references dbo.Bill(id),
+foreign key (idFood) references dbo.Food(id)
+);
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+INSERT INTO AccountType(id, name)
+VALUES(1, 'admin');
+INSERT INTO AccountType(id, name)
+VALUES(2, 'staff');
+
+INSERT INTO Account(UserName, password, DisplayName,Type)
+VALUES('admin', 'admin', N'Admin', 1);
+INSERT INTO Account(UserName, password, DisplayName,Type)
+VALUES('staff', 'staff', N'Staff', 2)
+
+INSERT INTO TableFood(name)
+VALUES('Bàn 1');
+INSERT INTO TableFood(name)
+VALUES('Bàn 2');
+INSERT INTO TableFood(name)
+VALUES('Bàn 3');
+INSERT INTO TableFood(name)
+VALUES('Bàn 4');
+INSERT INTO TableFood(name)
+VALUES('Bàn 5');
+INSERT INTO TableFood(name)
+VALUES('Bàn 6');
+INSERT INTO TableFood(name)
+VALUES('Bàn 7');
+INSERT INTO TableFood(name)
+VALUES('Bàn 8');
+INSERT INTO TableFood(name)
+VALUES('Bàn 9');
+INSERT INTO TableFood(name)
+VALUES('Bàn 10');
+
+INSERT INTO FoodCategory(name)
+VALUES('Cà phê');
+INSERT INTO FoodCategory(name)
+VALUES('Bánh');
+INSERT INTO FoodCategory(name)
+VALUES('Kem');
+INSERT INTO FoodCategory(name)
+VALUES('Trà');
+
+INSERT INTO Food(name, idCategory, price)
+VALUES('Espresso', 1, 30000);
+INSERT INTO Food(name, idCategory, price)
+VALUES('Latte', 1, 0);
+INSERT INTO Food(name, idCategory, price)
+VALUES('Đen đá', 1, 15000);
+INSERT INTO Food(name, idCategory, price)
+VALUES('Americano', 1, 40000);
+INSERT INTO Food(name, idCategory, price)
+VALUES('Cappuccino', 1, 40000);
+INSERT INTO Food(name, idCategory, price)
+VALUES('Sernik', 2, 25000);
+INSERT INTO Food(name, idCategory, price)
+VALUES('Kladdkaka', 2, 32000);
+INSERT INTO Food(name, idCategory, price)
+VALUES('Medovik', 2, 36000);
+INSERT INTO Food(name, idCategory, price)
+VALUES('Bánh kếp', 2, 27000);
+INSERT INTO Food(name, idCategory, price)
+VALUES('Donut', 2, 20000);
+INSERT INTO Food(name, idCategory, price)
+VALUES('Gelato', 3, 15000);
+INSERT INTO Food(name, idCategory, price)
+VALUES('Sorbet', 3, 18000);
+INSERT INTO Food(name, idCategory, price)
+VALUES('Sherbet', 3, 16000);
+INSERT INTO Food(name, idCategory, price)
+VALUES('Kem sữa chua', 3, 10000);
+INSERT INTO Food(name, idCategory, price)
+VALUES('Milkshakes', 3, 20000);
+INSERT INTO Food(name, idCategory, price)
+VALUES('Trà sữa', 4, 15000);
+INSERT INTO Food(name, idCategory, price)
+VALUES('Ô long', 4, 22000);
+INSERT INTO Food(name, idCategory, price)
+VALUES('Hồng trà', 4, 17000);
+INSERT INTO Food(name, idCategory, price)
+VALUES('Trà chanh', 4, 10000);
+INSERT INTO Food(name, idCategory, price)
+VALUES('Trà sen', 4, 24000);
+
+
+ 
